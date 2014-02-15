e2e43f1de8996aba43dc21c995fb3cf9717dafba
Joseph Utecht <thenmal@gmail.com>
2014-02-14 19:36:17 -0600

    node and redis working

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ var app = require('http').createServer(handler)
 app.listen(4000);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+  if(req.url === '/'){
+    req.url = '/index.html';
+  }
+  fs.readFile(__dirname + req.url,
   function (err, data) {
     if (err) {
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      return res.end('Error loading ' + req.url);
     }
 
     res.writeHead(200);
     res.end(data);
   });
 }
-/*
+
 var redis = require('redis');
 var red = redis.createClient();
 
@@ -24,11 +27,13 @@ red.on('error', function(err){
     console.log("Redis Error " + err);
 });
 
-red.subscribe('buttons');
-*/
+
 io.sockets.on('connection', function(socket){
-    socket.on('button_press', function(button){
+    socket.on('button_pressed', function(button){
         console.log(button);
-        //red.incr(button); 
+        red.hincrby("button", button, "1"); 
+        red.hget("button", button, function (err, reply) {
+            console.log(reply.toString());
+        });
     });
 });
