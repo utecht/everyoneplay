ff86c22a816313fa56f794202d477dff4a145631
Joseph Utecht <joseph.utecht@gmail.com>
2014-02-14 22:00:41 -0600

    Added keysending

--- eater.py
+++ eater.py
@@ -1,4 +1,6 @@
 import redis
+import time
+import os
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -15,6 +17,11 @@ def get_highest():
     return highest
 
 # here we need to a sleeping loop that calls xkeysend on get_highest()
+while True:
+    key = get_highest()
+    os.system("xdotool key '{}'".format(key))
+    time.sleep(.5)
+
 
 if __name__ == '__main__':
     print(get_highest())
