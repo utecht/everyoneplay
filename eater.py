999be7ee98511f5ec5a6b22b015e48f5328895e3
Joseph Utecht <thenmal@gmail.com>
2014-02-14 19:54:42 -0600

    python eater in-place

---
+++ eater.py
@@ -0,0 +1,18 @@
+import redis
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+def get_highest():
+    buttons = r.hgetall('button')
+    r.delete('button')
+
+    highest_num = 0
+    highest = None 
+    for b in buttons.keys():
+        if int(buttons[b]) > highest_num:
+            highest = b
+            highest_num = int(buttons[b])
+    return highest
+
+if __name__ == '__main__':
+    print(get_highest())
